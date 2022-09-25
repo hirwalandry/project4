4687b464ef4805832eadc69ae187a932e77bf896
nshuti_hirwa_landry <hirwanshuti40@gmail.com>
2022-09-25 21:09:49 +0200

    dev: update formModal14

--- src/App.js
+++ src/App.js
@@ -12,41 +12,11 @@ import "./App.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import AuthLoading from './views/AuthLoading';
-import { Provider } from "react-redux";
-import { FirebaseProvider, store } from "common";
-import { FirebaseConfig } from './config/FirebaseConfig';
-import { GoogleMapApiConfig } from "./config/GoogleMapApiConfig";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
 import { LoginPage, AddBookings } from "./views";
 
-i18n.use(initReactI18next).init({
-  resources: {},
-  fallbackLng: "en",
-  ns: ["translations"],
-  defaultNS: "translations",
-  interpolation: {
-    escapeValue: false,
-  },
-});
-
-function loadScript(src, position, id) {
-  if (!position) {
-    return;
-  }
-
-  const script = document.createElement("script");
-  script.setAttribute("async", "");
-  script.setAttribute("id", id);
-  script.src = src;
-  position.appendChild(script);
-}
-
-var hist = createBrowserHistory();
 
 function App() {
-  const { openModal, themeSettings, setThemeSettings, currentColor } =
+  const { openModal, setThemeSettings, currentColor } =
     useStateContext();
   useEffect(() => {
     AOS.init({
@@ -86,10 +56,10 @@ function App() {
         </Tooltip>
       </Box>
 
-      {themeSettings && <ThemeSettings />}
+      {/* {themeSettings && <ThemeSettings />} */}
 
       <Router>
-        <Routes history={hist}>
+        <Routes>
           <Route path="/" element={<MainComponent />}>
             <Route path="signin" element={<LoginPage />} />
             <Route path="addbookings" element={<AddBookings />} />

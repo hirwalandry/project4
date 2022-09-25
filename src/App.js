f17c9fe6b99b3c4d2762b8ef30ece1b1082072af
nshuti_hirwa_landry <hirwanshuti40@gmail.com>
2022-09-25 13:08:00 +0200

    dev-3

--- src/App.js
+++ src/App.js
@@ -1,66 +1,35 @@
-import React, {  useEffect } from "react";
-// import { createBrowserHistory } from "history";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { MainComponent } from "./views";
-// import appstore from "./assets/img/appstore.png";
-// import playstore from "./assets/img/playstore.png";
-// import { Box, IconButton, Tooltip } from "@mui/material";
-// import SettingsIcon from "@mui/icons-material/Settings";
-// import { useStateContext } from "./contexts/ContextProvider";
-// import { ThemeSettings } from "./components";
+import React, { useEffect } from "react";
+import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MainComponent } from "./views";
+import appstore from "./assets/img/appstore.png";
+import playstore from "./assets/img/playstore.png";
+import { Box, IconButton, Tooltip } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { useStateContext } from "./contexts/ContextProvider";
+import { ThemeSettings } from "./components";
 import "./App.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import { LoginPage, AddBookings } from "./views";
 
-// import AuthLoading from './views/AuthLoading';
-// import { Provider } from "react-redux";
-// import { FirebaseProvider, store } from "common";
-// import { FirebaseConfig } from './config/FirebaseConfig';
-// import { GoogleMapApiConfig } from "./config/GoogleMapApiConfig";
-// import i18n from "i18next";
-// import { initReactI18next } from "react-i18next";
-// import { LoginPage, AddBookings } from "./views";
-// i18n.use(initReactI18next).init({
-//   resources: {},
-//   fallbackLng: "en",
-//   ns: ["translations"],
-//   defaultNS: "translations",
-//   interpolation: {
-//     escapeValue: false,
-//   },
-// });
+i18n.use(initReactI18next).init({
+  resources: {},
+  fallbackLng: "en",
+  ns: ["translations"],
+  defaultNS: "translations",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-// function loadScript(src, position, id) {
-//   if (!position) {
-//     return;
-//   }
-
-//   const script = document.createElement("script");
-//   script.setAttribute("async", "");
-//   script.setAttribute("id", id);
-//   script.src = src;
-//   position.appendChild(script);
-// }
-
-// var hist = createBrowserHistory();
+var hist = createBrowserHistory();
 
 function App() {
-  // const { openModal, themeSettings, setThemeSettings, currentColor } =
-  //   useStateContext();
-  // const loaded = useRef(false);
-  // if (typeof window !== "undefined" && !loaded.current && !window.google) {
-  //   if (!document.querySelector("#google-maps")) {
-  //     loadScript(
-  //       "https://maps.googleapis.com/maps/api/js?key=" +
-  //         GoogleMapApiConfig +
-  //         "&libraries=geometry,drawing,places",
-  //       document.querySelector("head"),
-  //       "google-maps"
-  //     );
-  //   }
-  //   loaded.current = true;
-  // }
-
+  const { openModal, themeSettings, setThemeSettings, currentColor } =
+    useStateContext();
   useEffect(() => {
     AOS.init({
       once: true,
@@ -75,9 +44,8 @@ function App() {
     //   <FirebaseProvider config={FirebaseConfig} appcat={AppCat}>
     //     <AuthLoading>
     <>
-      {/* <Box position={"fixed"} left={20} bottom={20} style={{ zIndex: "1000" }}> */}
-      {/* <TooltipComponent content="Settings" position="Top"> */}
-      {/* <Box
+      <Box position={"fixed"} left={20} bottom={20} style={{ zIndex: "1000" }}>
+        <Box
           justifyContent="center"
           alignItems="center"
           xs={12}
@@ -109,10 +77,11 @@ function App() {
             <SettingsIcon style={{ color: "white" }} />
           </IconButton>
         </Tooltip>
-      </Box> */}
-      {/* {themeSettings && <ThemeSettings />} */}
+      </Box>
 
-      {/* <Router>
+      {themeSettings && <ThemeSettings />}
+
+      <Router>
         <Routes history={hist}>
           <Route path="/" element={<MainComponent />}>
             <Route path="signin" element={<LoginPage />} />
@@ -125,8 +94,7 @@ function App() {
             <Route path="/addbookings" element={<AddBookings />} />
           </Routes>
         ) : null}
-      </Router> */}
-      <h1>Hello world</h1>
+      </Router>
     </>
   );
 }

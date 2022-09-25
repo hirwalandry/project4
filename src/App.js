d018bca036f01542c3bff45ae8004f389d430c36
nshuti_hirwa_landry <hirwanshuti40@gmail.com>
2022-09-25 20:58:52 +0200

    dev: update formModal12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {  useEffect } from "react";
+import { createBrowserHistory } from "history";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { MainComponent } from "./views";
+import appstore from "./assets/img/appstore.png";
+import playstore from "./assets/img/playstore.png";
 import { Box, IconButton, Tooltip } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { useStateContext } from "./contexts/ContextProvider";
@@ -9,12 +12,38 @@ import "./App.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-
+import AuthLoading from './views/AuthLoading';
+import { Provider } from "react-redux";
+import { FirebaseProvider, store } from "common";
+import { FirebaseConfig } from './config/FirebaseConfig';
+import { GoogleMapApiConfig } from "./config/GoogleMapApiConfig";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 import { LoginPage, AddBookings } from "./views";
 
+i18n.use(initReactI18next).init({
+  resources: {},
+  fallbackLng: "en",
+  ns: ["translations"],
+  defaultNS: "translations",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
+function loadScript(src, position, id) {
+  if (!position) {
+    return;
+  }
 
+  const script = document.createElement("script");
+  script.setAttribute("async", "");
+  script.setAttribute("id", id);
+  script.src = src;
+  position.appendChild(script);
+}
 
+var hist = createBrowserHistory();
 
 function App() {
   const { openModal, themeSettings, setThemeSettings, currentColor } =
@@ -34,6 +63,7 @@ function App() {
     //     <AuthLoading>
     <>
       <Box position={"fixed"} left={20} bottom={20} style={{ zIndex: "1000" }}>
+      
       </Box>
       <Box position={"fixed"} right={10} bottom={20} style={{ zIndex: "1000" }}>
         <Tooltip title="Setting" placement="top" arrow>
@@ -59,7 +89,7 @@ function App() {
       {themeSettings && <ThemeSettings />}
 
       <Router>
-        <Routes>
+        <Routes history={hist}>
           <Route path="/" element={<MainComponent />}>
             <Route path="signin" element={<LoginPage />} />
             <Route path="addbookings" element={<AddBookings />} />

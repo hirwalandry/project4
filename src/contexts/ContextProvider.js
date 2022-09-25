b1ed297b927435c728d899115b6841e80014141b
nshuti_hirwa_landry <hirwanshuti40@gmail.com>
2022-09-25 14:38:35 +0200

    dev-7

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -54,20 +54,20 @@ export const ContextProvider = ({ children }) => {
     setThemeSettings(false);
   };
 
-  // useEffect(() => {
-  //   try {
-  //     const localTheme = window.localStorage.getItem("themeMode");
-  //     const localColor = window.localStorage.getItem("colorsMode");
-  //     localTheme ? setMode(localTheme) : colorMode.toggleColorMode("light");
-  //     localColor ? setColor(localColor) : setCurrentColor("red");
-  //   } catch {
-  //     colorMode.toggleColorMode("light");
-  //     setCurrentColor("red");
-  //   }
+  useEffect(() => {
+    try {
+      const localTheme = window.localStorage.getItem("themeMode");
+      // const localColor = window.localStorage.getItem("colorsMode");
+      localTheme ? setMode(localTheme) : colorMode.toggleColorMode("light");
+      // localColor ? setColor(localColor) : setCurrentColor("red");
+    } catch {
+      colorMode.toggleColorMode("light");
+      // setCurrentColor("red");
+    }
 
-  //   // setMountedComponent(true);
-  //   // eslint-disable-next-line
-  // }, []);
+    // setMountedComponent(true);
+    // eslint-disable-next-line
+  }, []);
 
   useEffect(() => {
     // Remove the server-side injected CSS.

c7af4bde1d6d6aea878f2f3274e2da7ce4d9c00b
nshuti_hirwa_landry <hirwanshuti40@gmail.com>
2022-09-25 21:24:32 +0200

    dev: update formModal18

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import  Box  from '@mui/material/Box';
+import Box from "@mui/material/Box";
 
 function Input({ font, label, name, errors, ...rest }) {
   return (
-    <Box marginBottom={2}>
-      <TextField {...rest} id={name} label={label} name={name} variant="standard" fullWidth/>
+    <Box marginBottom={2} minWidth={400}>
+      <TextField
+        {...rest}
+        id={name}
+        label={label}
+        name={name}
+        variant="standard"
+        fullWidth
+      />
       {errors && <Box className="alert alert-warning">{errors}</Box>}
     </Box>
   );
